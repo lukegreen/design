8848b8f9ac3fba278ca3f176497c86ec7942e28e
Luke Green <elukuent@gmail.com>
2017-03-17 11:15:27 -0700

    roughing in styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,19 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+$(document).ready(function(){
+
+  /*Change active class for menu items*/
+
+  $('.nav li').on('click', function(){
+    $('.nav li').removeClass('active');
+    $(this).addClass('active');
+  });
+
+  /*Skill box interactivity*/
+
+  $('.skill-box').hover(function(){
+    $(this).children('h2').fadeOut(500);
+
+  }, function(){
+    $(this).children('h2').fadeIn();
+  });
+
+})
